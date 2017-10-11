e9926b160328ca8569c35cfc1771e14ceae4dd13
傅文松 <908159107@qq.com>
2017-10-11 23:19:59 +0800

    add suite api module

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-// 微信企业号主动调用接口API
-const API = require('./lib/api_common')
+const AccessToken = require('./lib/core/access-token')
+const API = require('./lib/core/api')
+const CorpAPI = require('./lib/core/corp-api')
+const SuiteAPI = require('./lib/core/suite-api')
 
+/*** 微信企业应用 ***/
 // 授权
-API.mixin(require('./lib/api_oauth'))
+CorpAPI.mixin(require('./lib/corp_oauth'))
 
+/*** 微信企业服务商 ***/
+// 第三方
+SuiteAPI.mixin(require('./lib/suite_3rd'))
+
+module.exports.AccessToken = AccessToken
 module.exports.API = API
+module.exports.CorpAPI = CorpAPI
+module.exports.SuiteAPI = SuiteAPI
