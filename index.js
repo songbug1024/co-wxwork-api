2710688626d702373737a6cce42aa520f9ace2a6
Fu Wen Song <908159107@qq.com>
2017-10-13 18:53:27 +0800

    add corp jsapi, fix some bug

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const API = require('./lib/core/api')
 const CorpAPI = require('./lib/core/corp-api')
 const SuiteAPI = require('./lib/core/suite-api')
 
-/*** 微信企业应用 ***/
+/*  微信企业应用 */
 // 授权
 CorpAPI.mixin(require('./lib/corp_oauth'))
+// js API
+CorpAPI.mixin(require('./lib/corp_js'))
 
-/*** 微信企业服务商 ***/
+/* 微信企业服务商 */
 // 第三方
 SuiteAPI.mixin(require('./lib/suite_3rd'))
 
